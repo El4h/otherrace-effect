73abf5b1b96dc67adbb5787271dd17194698732b
El4h <a.elaheh69@gmail.com>
2023-03-22 17:11:46 +0100

    slightly modified

--- sdnn-otherrace/lesioning/lesion.sh
+++ sdnn-otherrace/lesioning/lesion.sh
@@ -32,9 +32,9 @@
 #NONSORT_TASK_INDEX=1
 
 
-SORT_TASK_INDEX=$1
+SORT_TASK_INDEX=$1  #can be 0 or 1
 NONSORT_TASK_INDEX=$2
-PARAM_GROUP_INDEX=$3 # 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 # for vgg16
+PARAM_GROUP_INDEX=$12 # 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 # for vgg16 what layer we are lesioning
 
 
 #CONFIG_FILE='./configs/vgg/face_inanimate_400k_seed.yaml'
@@ -42,8 +42,8 @@ PARAM_GROUP_INDEX=$3 # 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 # for vgg16
 CONFIG_FILE='/om2/user/juliom/projects/moco/configs/moco1_vgg16_classifier_transfer_from_saycam_to_facecar_inanimate1.json'
 
 
-GREEDY_P=0.2  # 0.2
-GROUP_P=0.016 # 0.016
+GREEDY_P=0.25  # 0.2
+GROUP_P=0.016 # 0.016 , corresponds to 8 filters at the last layer 512 * 0.016
 NGPUS=1
 BATCH_SIZE=128
 MAX_BATCHES=50
@@ -106,7 +106,7 @@ echo
 echo 'submitting python script...'
 echo
 
-umask 0002
+# umask 0002
 
 CUDA_VISIBLE_DEVICES=0 python lesion.py --config_file $CONFIG_FILE --param_group_index $PARAM_GROUP_INDEX --greedy_p $GREEDY_P --group_p $GROUP_P --shuffle $SHUFFLE --ngpus $NGPUS --batch_size $BATCH_SIZE --max_batches $MAX_BATCHES --sort_task_index $SORT_TASK_INDEX --nonsort_task_index $NONSORT_TASK_INDEX --restore_epoch $RESTORE_EPOCH --lesion_name $LESION_NAME --iterator_seed $ITER_SEED_TYPE --maxout $MAXOUT --read_seed $READ_SEED --randomize_classes $RAND_CLASSES --randomize_classes_seed $RAND_CLASSES_SEED
 # --subgroups_file $SUBGROUPS_FILE # -
