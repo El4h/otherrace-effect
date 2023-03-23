cabe866b1d97449950b0784a0c21bf08e0680865
El4h <a.elaheh69@gmail.com>
2023-03-23 18:53:35 +0100

    changed directories, created yaml and output file

--- sdnn-otherrace/lesioning/lesion.sh
+++ sdnn-otherrace/lesioning/lesion.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=1-00:00:00
 #SBATCH -p nklab
 #SBATCH --mail-type=END
-#SBATCH --mail-user=juliom@mit.edu
+#SBATCH --mail-user=elahakbri@gmail.com
 #SBATCH --array=0
 #SBATCH --output='./output/%A_%a.out'
 #SBATCH --gres=gpu:1 # QUADRORTX6000:1 # GEFORCERTX2080TI:1 # gres=gpu:1
@@ -33,13 +33,13 @@
 
 
 SORT_TASK_INDEX=$1  #can be 0 or 1
-NONSORT_TASK_INDEX=$2
+NONSORT_TASK_INDEX=$0
 PARAM_GROUP_INDEX=$12 # 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 # for vgg16 what layer we are lesioning
 
 
-#CONFIG_FILE='./configs/vgg/face_inanimate_400k_seed.yaml'
+CONFIG_FILE='./configs/vgg/face_dual_whitasia.yaml'
 #CONFIG_FILE='/om2/user/juliom/projects/moco/configs/moco1_vgg16_transfer_classifier_facecar_inanimate1.json'
-CONFIG_FILE='/om2/user/juliom/projects/moco/configs/moco1_vgg16_classifier_transfer_from_saycam_to_facecar_inanimate1.json'
+#CONFIG_FILE='/om2/user/juliom/projects/moco/configs/moco1_vgg16_classifier_transfer_from_saycam_to_facecar_inanimate1.json'
 
 
 GREEDY_P=0.25  # 0.2
@@ -48,7 +48,7 @@ NGPUS=1
 BATCH_SIZE=128
 MAX_BATCHES=50
 RESTORE_EPOCH=-1
-LESION_NAME='trial1' 
+LESION_NAME='trial12' 
 ITER_SEED_TYPE='selection'
 READ_SEED=0
 MAXOUT='True'
@@ -108,6 +108,6 @@ echo
 
 # umask 0002
 
-CUDA_VISIBLE_DEVICES=0 python lesion.py --config_file $CONFIG_FILE --param_group_index $PARAM_GROUP_INDEX --greedy_p $GREEDY_P --group_p $GROUP_P --shuffle $SHUFFLE --ngpus $NGPUS --batch_size $BATCH_SIZE --max_batches $MAX_BATCHES --sort_task_index $SORT_TASK_INDEX --nonsort_task_index $NONSORT_TASK_INDEX --restore_epoch $RESTORE_EPOCH --lesion_name $LESION_NAME --iterator_seed $ITER_SEED_TYPE --maxout $MAXOUT --read_seed $READ_SEED --randomize_classes $RAND_CLASSES --randomize_classes_seed $RAND_CLASSES_SEED
+CUDA_VISIBLE_DEVICES=4 python3 lesion.py --config_file $CONFIG_FILE --param_group_index $PARAM_GROUP_INDEX --greedy_p $GREEDY_P --group_p $GROUP_P --shuffle $SHUFFLE --ngpus $NGPUS --batch_size $BATCH_SIZE --max_batches $MAX_BATCHES --sort_task_index $SORT_TASK_INDEX --nonsort_task_index $NONSORT_TASK_INDEX --restore_epoch $RESTORE_EPOCH --lesion_name $LESION_NAME --iterator_seed $ITER_SEED_TYPE --maxout $MAXOUT --read_seed $READ_SEED --randomize_classes $RAND_CLASSES --randomize_classes_seed $RAND_CLASSES_SEED > output.txt
 # --subgroups_file $SUBGROUPS_FILE # -
 
